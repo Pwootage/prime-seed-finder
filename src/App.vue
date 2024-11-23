70f1742763f1165412c1091bd318cd78ac2dfde3
Pwootage <pwootage@gmail.com>
2024-11-22 20:13:35 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink>
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+
+</style>
